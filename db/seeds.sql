7251f71aa8f9bb3cb62b2a3f39a2bfcb1d05f788
prototype1309 <simo.hourri@gmail.com>
2023-08-01 21:51:21 -0400

    seeding the daba, adding the value and inserting them

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (name)
+VALUES ("Sales"),
+       ("Engineering"),
+       ("Finance"),
+       ("Legal");
+
+INSERT INTO (title, salary, department_id)
+VALUES  ("Sales Lead", 100000, 1),
+        ("Salesperson", 80000, 1),
+        ("Lead Engineer", 150000, 2),
+        ("Software Engineer", 120000, 2),
+        ("Account Manager", 160000, 3),
+        ("Accountant", 125000, 3),
+        ("Legal Team Lead", 250000, 4),
+        ("Lawyer", 190000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ("John", "Doe", 1, null),
+        ("Mohamed", "hourri", 2, 1),
+        ("kaylee", "Middleton", 3, null),
+        ("Kevin", "Greek", 4, 3),
+        ("Simo", "Boukhriss", 5, null),
+        ("Mouad", "Alajali", 6, 5),
+        ("Sarah", "Lourd", 7, null),
+        ("Tom", "Jerry", 8, 7);       
