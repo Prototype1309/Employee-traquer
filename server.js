b53b632671ca6ac4e2266ca412890cb3dabb247b
prototype1309 <simo.hourri@gmail.com>
2023-08-01 22:26:38 -0400

    viewing tables functions

--- server.js
+++ server.js
@@ -83,3 +83,30 @@ function viewDepartments() {
         startingQuestion();
     });
 };
+
+
+function viewRoles() {
+    const sql = `SELECT role.id, role.title AS role, role.salary, department.name AS department FROM role INNER JOIN department ON (department.id = role.department_id);`;
+    db.query(sql, (err, res ) => {
+        if(err) {
+            console.log(err);
+            return;
+        }
+        console.table(res);
+        startingQuestion();
+
+    });
+};
+
+function vienEmployees() {
+    const sql = `SELECT employee.id, employee.first_name, employee.last_name, role.title AS role, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN employee manager on manager.id = employee.manager_id INNER JOIN role ON (role.id = employee.role_id) INNER JOIN department ON (department.id = role.department_id) ORDER BY employee.id;`;
+     db.query(sql, (err, res) => {
+       if (err) {
+         console.log(err);
+         return;
+       }
+       console.table(res);
+       startingQuestion();
+     });
+};
+
